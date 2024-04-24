7ae7c752c450829f2facebebf8410cadba1a125f
Enrique Macias Lopez <a01641402@tec.mx>
2024-04-24 16:00:51 -0600

    creacion gestion-usarios js

---
+++ js/gestion-usuarios.js
@@ -0,0 +1,5 @@
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import {  getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js"
+import {app} from './firebase.js'
+
+const db = getFirestore(app);
