c3bade78cb2d6a8f23101b9c3e82c229ac031cf0
diego <diegolarrieta@gmail.com>
2024-04-24 18:42:53 -0600

    gestionarCursos

--- js/gestionarCursos.js
+++ js/gestionarCursos.js
@@ -3,73 +3,55 @@ import {  getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/fireba
 import {app} from './firebase.js'
 
 const db = getFirestore(app);
-const auth = getAuth();
+const auth = getAuth(app);
 
-const editCourses = document.getElementById('addCourseForm');
+const addCourseForm = document.getElementById('editCourseForm');
 
-const newCourseRef = firebase.firestore().collection('courses').doc(); 
-const id = newCourseRef.id; 
-newCourseRef.set({ /* datos del curso */ });
-
-
-
-function editCourses(courseId) {
-    const editCourseForm = document.getElementById('addCourseForm');
-    editCourseForm.setAttribute('data-course-id', courseId);
-  
-    // Aquí deberías cargar los datos del curso en los campos del formulario
-    // Utiliza firebase.firestore().collection('courses').doc(courseId).get() para obtener los datos
-  }
-
-document.getElementById('editCourseForm').addEventListener('submit', async (e) => {
+addCourseForm.addEventListener('submit', async (e) => {
     e.preventDefault(); // Esto evitará que el formulario se envíe y agregue los datos a la URL
 
-    // Obtén el ID del curso que estás editando
-    const courseId = e.target.getAttribute('data-course-id');
-    
-    // Obtén los valores del formulario
-    const courseName = document.getElementById('editCourseName').value;
-    const courseDescription = document.getElementById('editCourseDescription').value;
-    const courseCategory = document.getElementById('editCourseCategory').value;
-    const courseLink = document.getElementById('editLearningObjectives').value;
-    const coursePublication = document.getElementById('toggleCoursePublication').checked;
+    const courseNameInput = document.getElementById('editCourseName');
+    const courseDescriptionInput = document.getElementById('editCourseDescription');
+    const courseCategoryInput = document.getElementById('editCourseCategory');
+    const courseLinkInput = document.getElementById('editLearningObjective');
+    const coursePublishInput = document.getElementById('toggleCoursePublication')
 
-    // Asegúrate de que el ID del curso esté presente
-    if (courseId) {
-        try {
-            // Actualizar el curso en Firestore
-            await firebase.firestore().collection('courses').doc(courseId).update({
-                nombre: courseName,
-                descripcion: courseDescription,
-                categoria: courseCategory,
-                hipervinculo: courseLink,
-                publicado: coursePublication,
-                fechaEdicion: new Date() // o firebase.firestore.FieldValue.serverTimestamp()
-            });
-            console.log('Curso actualizado con éxito');
-            const successMessage = document.createElement('div');
-            successMessage.textContent = 'Creacion de curso exitoso';
-            successMessage.className = 'success-message'; // Asegúrate de definir este estilo en tu CSS
-            document.body.appendChild(successMessage);
-    
-            // Eliminar el mensaje después de 2 segundos
-            setTimeout(() => {
+    const name = courseNameInput.value;
+    const description = courseDescriptionInput.value;
+    const category = courseCategoryInput.value;
+    const link = courseLinkInput.value;
+    const publish = coursePublishInput.value;
+
+    try{
+
+        // Guarda la información adicional del usuario en Firestore
+        await setDoc(doc(db, "courses", userCredential.user.uid), {
+            name: name,
+            description: description,
+            category: category,
+            link:link,
+            publish:publish,
+        });
+
+        console.log('Courses information saved to Firestore');
+
+        const successMessage = document.createElement('div');
+        successMessage.textContent = 'Creación de cuenta exitosa';
+        successMessage.className = 'success-message'; // Utiliza esta clase para el estilo en tu CSS
+        document.body.appendChild(successMessage);
+
+        setTimeout(() => {
+            if (document.body.contains(successMessage)) {
                 document.body.removeChild(successMessage);
-            }, 2000);
-    
-            setTimeout(() => {
-                window.location.href = 'index.html';
-            }, 2500); // Asegúrate de que este retraso sea mayor al del mensaje para que el usuario pueda leerlo
-    
-            signinForm.reset()
-        } catch (error) {
-            console.error('Error al actualizar el curso: ', error);
-            // Maneja el error, por ejemplo, mostrando un mensaje al usuario
-            // ...
-        }
-    } else {
-        console.error('No se proporcionó el ID del curso para la actualización');
-        // Maneja la falta de ID de curso aquí, tal vez mostrando un error al usuario
-        // ...
+            }
+        }, 3000);
+
+
+
+        signupForm.reset();
+    }catch (error) {
+        console.error('Error during creating a course:', error);
+        // Manejo del error de registro
     }
+
 });
--- js/index.js
+++ js/index.js
@@ -1,78 +1,71 @@
 /*
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import {app} from './firebase.js'
+import { app } from './firebase.js'
 
 const db = getFirestore(app);
 const auth = getAuth();
 
-
-
-// index.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    function updateIndexProfilePicture(user) {
-        const userDocRef = doc(db, "users", user.uid);
-    
-        getDoc(userDocRef).then((docSnap) => {
-            if (docSnap.exists()) {
-                const userData = docSnap.data();
-                if (userData.profilePicture) {
-                    document.getElementById('indexProfileImg').src = userData.profilePicture;
-                }
-            } else {
-                console.log("No se encontraron datos del usuario.");
-            }
-        }).catch((error) => {
-            console.error("Error al obtener datos de usuario", error);
-        });
+// Define la función typeWriter dentro del contexto donde se usa
+function typeWriter(fullText, elementId, typingSpeed) {
+    let currentIndex = 0;
+    const element = document.getElementById(elementId);
+    if (!element) {
+        console.error(`Element with ID '${elementId}' not found.`);
+        return;
     }
-    // Toda tu lógica que depende del DOM va aquí, como:
-    // Define la función fuera del onAuthStateChanged para poder llamarla después
-    function typeWriter(fullText, elementId, typingSpeed) {
-        let currentIndex = 0;
-        const element = document.getElementById(elementId);
-        element.textContent = ''; // Iniciar con el contenido vacío
-    
-        const typeNextLetter = () => {
+    element.textContent = ''; // Iniciar con el contenido vacío
+  
+    function typeNextLetter() {
         if (currentIndex < fullText.length) {
-            // Agregar la siguiente letra al contenido actual
             element.textContent += fullText.charAt(currentIndex);
-            // Incrementar el índice para la siguiente letra
             currentIndex++;
-            // Llamar a la función nuevamente después de un cierto tiempo
             setTimeout(typeNextLetter, typingSpeed);
         }
-        };
-    
-        // Iniciar la animación de escritura
-        typeNextLetter();
     }
+  
+    typeNextLetter();
+}
+
+function updateIndexProfilePicture(user) {
+    const userDocRef = doc(db, "users", user.uid);
+
+    getDoc(userDocRef).then((docSnap) => {
+        if (docSnap.exists()) {
+            const userData = docSnap.data();
+            if (userData.profilePicture) {
+                document.getElementById('indexProfileImg').src = userData.profilePicture;
+            }
+        } else {
+            console.log("No se encontraron datos del usuario.");
+        }
+    }).catch((error) => {
+        console.error("Error al obtener datos de usuario", error);
+    });
+}
 
     function updateIndexUserName(user) {
         const userDocRef = doc(db, "users", user.uid);
 
-        getDoc(userDocRef).then((docSnap) => {
-            if (docSnap.exists()) {
-                const userData = docSnap.data();
-                if (userData.name) {
-                    const fullName = `Bienvenido, ${userData.name}`;
-                    document.getElementById('welcomeText').textContent = `Bienvenido, ${userData.name}!`;
-                    typeWriter(fullName, 'welcomeText', 150);
-                }
-            } else {
-                console.log("No se encontraron datos del usuario.");
+    getDoc(userDocRef).then((docSnap) => {
+        if (docSnap.exists()) {
+            const userData = docSnap.data();
+            if (userData.name) {
+                typeWriter(`Bienvenido, ${userData.name}`, 'welcomeText', 150);
             }
-        }).catch((error) => {
-            console.error("Error al obtener datos de usuario", error);
-        });
-    }
-
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            updateIndexProfilePicture(user);
-            updateIndexUserName(user);
+        } else {
+            console.log("No se encontraron datos del usuario.");
         }
+    }).catch((error) => {
+        console.error("Error al obtener datos de usuario", error);
     });
-    
+}
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        updateIndexProfilePicture(user);
+        updateIndexUserName(user);
+    } else {
+        // Handle user not logged in or not found
+    }
 });
-*/
