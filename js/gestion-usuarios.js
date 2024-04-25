c964059290abbd9aca45996df2b7bc5cfcea7014
Enrique Macias Lopez <a01641402@tec.mx>
2024-04-24 18:05:58 -0600

    fixes and gestion usuarios added

--- js/gestion-usuarios.js
+++ js/gestion-usuarios.js
@@ -1,5 +1,27 @@
-import { doc, setDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
-import {  getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js"
-import {app} from './firebase.js'
+import { collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { app } from './firebase.js';
 
 const db = getFirestore(app);
+
+// Asegúrate de que el código se ejecute solo después de que el DOM esté cargado
+document.addEventListener('DOMContentLoaded', () => {
+  // Referencia a la colección de usuarios
+  const usersCollectionRef = collection(db, "users");
+
+  // Escuchar cambios en la colección de usuarios
+  onSnapshot(usersCollectionRef, (snapshot) => {
+    // Contar el total de usuarios
+    const totalUsuarios = snapshot.size;
+
+    // Actualizar el HTML con el total de usuarios
+    const totalUsuariosElement = document.getElementById('totalUsuarios');
+    if (totalUsuariosElement) {
+        totalUsuariosElement.textContent = totalUsuarios;
+    } else {
+        console.error('Elemento totalUsuarios no encontrado');
+    }
+  }, (error) => {
+    console.error("Error al obtener datos de usuarios para el totalUsuario:", error);
+  });
+});
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import {app} from './firebase.js'
@@ -5,66 +6,73 @@ import {app} from './firebase.js'
 const db = getFirestore(app);
 const auth = getAuth();
 
-function updateIndexProfilePicture(user) {
-    const userDocRef = doc(db, "users", user.uid);
 
-    getDoc(userDocRef).then((docSnap) => {
-        if (docSnap.exists()) {
-            const userData = docSnap.data();
-            if (userData.profilePicture) {
-                document.getElementById('indexProfileImg').src = userData.profilePicture;
+
+// index.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    function updateIndexProfilePicture(user) {
+        const userDocRef = doc(db, "users", user.uid);
+    
+        getDoc(userDocRef).then((docSnap) => {
+            if (docSnap.exists()) {
+                const userData = docSnap.data();
+                if (userData.profilePicture) {
+                    document.getElementById('indexProfileImg').src = userData.profilePicture;
+                }
+            } else {
+                console.log("No se encontraron datos del usuario.");
             }
-        } else {
-            console.log("No se encontraron datos del usuario.");
-        }
-    }).catch((error) => {
-        console.error("Error al obtener datos de usuario", error);
-    });
-}
-
-// Define la función fuera del onAuthStateChanged para poder llamarla después
-function typeWriter(fullText, elementId, typingSpeed) {
-    let currentIndex = 0;
-    const element = document.getElementById(elementId);
-    element.textContent = ''; // Iniciar con el contenido vacío
-  
-    const typeNextLetter = () => {
-      if (currentIndex < fullText.length) {
-        // Agregar la siguiente letra al contenido actual
-        element.textContent += fullText.charAt(currentIndex);
-        // Incrementar el índice para la siguiente letra
-        currentIndex++;
-        // Llamar a la función nuevamente después de un cierto tiempo
-        setTimeout(typeNextLetter, typingSpeed);
-      }
-    };
-  
-    // Iniciar la animación de escritura
-    typeNextLetter();
-  }
-
-function updateIndexUserName(user) {
-    const userDocRef = doc(db, "users", user.uid);
-
-    getDoc(userDocRef).then((docSnap) => {
-        if (docSnap.exists()) {
-            const userData = docSnap.data();
-            if (userData.name) {
-                const fullName = `Bienvenido, ${userData.name}`;
-                //document.getElementById('welcomeText').textContent = `Bienvenido, ${userData.name}`;
-                typeWriter(fullName, 'welcomeText', 150);
-            }
-        } else {
-            console.log("No se encontraron datos del usuario.");
-        }
-    }).catch((error) => {
-        console.error("Error al obtener datos de usuario", error);
-    });
-}
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        updateIndexProfilePicture(user);
-        updateIndexUserName(user);
+        }).catch((error) => {
+            console.error("Error al obtener datos de usuario", error);
+        });
     }
+    // Toda tu lógica que depende del DOM va aquí, como:
+    // Define la función fuera del onAuthStateChanged para poder llamarla después
+    function typeWriter(fullText, elementId, typingSpeed) {
+        let currentIndex = 0;
+        const element = document.getElementById(elementId);
+        element.textContent = ''; // Iniciar con el contenido vacío
+    
+        const typeNextLetter = () => {
+        if (currentIndex < fullText.length) {
+            // Agregar la siguiente letra al contenido actual
+            element.textContent += fullText.charAt(currentIndex);
+            // Incrementar el índice para la siguiente letra
+            currentIndex++;
+            // Llamar a la función nuevamente después de un cierto tiempo
+            setTimeout(typeNextLetter, typingSpeed);
+        }
+        };
+    
+        // Iniciar la animación de escritura
+        typeNextLetter();
+    }
+
+    function updateIndexUserName(user) {
+        const userDocRef = doc(db, "users", user.uid);
+
+        getDoc(userDocRef).then((docSnap) => {
+            if (docSnap.exists()) {
+                const userData = docSnap.data();
+                if (userData.name) {
+                    const fullName = `Bienvenido, ${userData.name}`;
+                    document.getElementById('welcomeText').textContent = `Bienvenido, ${userData.name}!`;
+                    typeWriter(fullName, 'welcomeText', 150);
+                }
+            } else {
+                console.log("No se encontraron datos del usuario.");
+            }
+        }).catch((error) => {
+            console.error("Error al obtener datos de usuario", error);
+        });
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            updateIndexProfilePicture(user);
+            updateIndexUserName(user);
+        }
+    });
+    
 });
+*/
